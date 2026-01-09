042e177c5fc6de11048e0a454e2a4cbe66b51c42
astikspandey <astikspandey@gmail.com>
2026-01-09 10:34:04 +0530

    Fixed CSS loading

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
+
+// CSS is built separately with Tailwind CLI and linked in index.html
+// import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
